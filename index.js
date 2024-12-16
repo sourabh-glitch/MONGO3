f0e25ce308d1193955bc93747fa89d17bcb14e91
sourabh-glitch <upadhaysourav14@gmail.com>
2024-12-17 03:31:26 +0530

    dbmodel

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chat.js")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs")
@@ -14,6 +15,20 @@ main()
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatapp');
 }  
+
+let chat1= new Chat({
+    from:"neha",
+    to: "sourabh",
+    msg: "How are yoy ",
+    created_at: new Date()
+});
+
+chat1.save().then(res =>{
+    console.log(res)
+})
+.catch(err => {
+    console.log(err)
+});
 
 app.get("/", (req, res) => {
 
